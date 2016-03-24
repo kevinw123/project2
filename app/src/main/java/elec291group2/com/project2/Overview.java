219d3522e46083be3ac12eeaf150b268caa12f9d
Kevin <kevinqiu96@gmail.com>
2016-03-24 00:32:23 -0700

    changed some stuff in overview

--- app/src/main/java/elec291group2/com/project2/Overview.java
+++ app/src/main/java/elec291group2/com/project2/Overview.java
@@ -6,16 +6,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Kevin on 2016-03-23.
  */
 public class Overview extends Fragment
 {
+    View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.overview, container, false);
+        view = inflater.inflate(R.layout.settings, container, false);
+        TextView overview = (TextView) view.findViewById(R.id.overview);
+        overview.setText("Test");
+
+        return view;
     }
 }
