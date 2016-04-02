4d801bdb2062b56991150f60b407a9bc05ffef66
Kevin Wong <kevinwong_895@hotmail.com>
2016-04-02 00:31:59 -0700

    Camera working on app

--- app/src/main/java/elec291group2/com/project2/Camera.java
+++ app/src/main/java/elec291group2/com/project2/Camera.java
@@ -3,23 +3,59 @@ package elec291group2.com.project2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
+import android.app.Activity;
+import android.widget.Toast;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Menu;
 
-public class Camera extends AppCompatActivity
+public class Camera extends Activity
 {
+    private static final String TAG = "Camera";
+    private String path;
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.camera);
+
+        if (!LibsChecker.checkVitamioLibs(this))
+            return;
+
         setContentView(R.layout.camera);
-        getSupportActionBar().setTitle("Camera");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setTitle("Camera");
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mVideoView = (VideoView) findViewById(R.id.surface_view);
+        path = "rtmp://192.168.1.71:1935/live/myStream";
+
+
+
+        mVideoView.setVideoPath(path);
+        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.requestFocus();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // optional need Vitamio 4.0
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
+        });
     }
 
 
-    @Override
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId())
         {
@@ -36,5 +72,5 @@ public class Camera extends AppCompatActivity
     {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
-    }
+    }*/
 }
